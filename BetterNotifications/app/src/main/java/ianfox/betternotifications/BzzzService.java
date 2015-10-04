0270e68ea8e2181db8beb22df586328d5b3e25a9
Ian Fox <ianfox97@gmail.com>
2015-10-04 19:31:54 -0400

    I broke everything

--- BetterNotifications/app/src/main/java/ianfox/betternotifications/BzzzService.java
+++ BetterNotifications/app/src/main/java/ianfox/betternotifications/BzzzService.java
@@ -1,28 +1,35 @@
 package ianfox.betternotifications;
 
-import android.annotation.TargetApi;
-import android.app.IntentService;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
-import android.os.Build;
 import android.os.IBinder;
-import android.os.PowerManager;
-import android.os.Vibrator;
-import android.app.Notification;
-import android.support.annotation.Nullable;
 import java.util.Random;
 
 
-import android.util.Log;
+import android.support.annotation.Nullable;
 
 /**
  * Created by Ian on 2015-10-04.
  */
-public class BzzzService extends IntentService {
+public class BzzzService extends Service {
     public BzzzService() {
-        super("Bzzz");
+    }
+    Alarm alarm = new Alarm();
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        alarm.SetAlarm(this);
+        return START_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 
     public class SBinder extends Binder {
@@ -31,25 +38,6 @@ public class BzzzService extends IntentService {
         }
     }
 
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-        Random r = new Random();
-        while (true) {
-            if (!pm.isScreenOn()){
-                v.vibrate(1000);
-            }
-            sleep(r.nextInt(20000) + 10000);
-        }
-    }
-
-    public static void sleep(int duration) {
-        try {
-            Thread.sleep(duration);
-        } catch (Exception e) {
-        }
-    }
 
 
 }
--- BetterNotifications/app/src/main/java/ianfox/betternotifications/MainActivity.java
+++ BetterNotifications/app/src/main/java/ianfox/betternotifications/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.IBinder;
+
 
 public class MainActivity extends AppCompatActivity {
 
